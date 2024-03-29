b14574e680f5bacb68347ffb2b8b372735d1e428
SAEED.PANAHI <panahi.projects@gmail.com>
2024-03-29 11:36:47 +0330

    Improve the loading experience

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -10,13 +10,8 @@ import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-// import SimpleMDE from "react-simplemde-editor";
-import dynamic from "next/dynamic";
+import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
-
-const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-  ssr: false,
-});
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,12 +1,2 @@
-import { Box } from "@radix-ui/themes";
-import { Skeleton } from "@/app/components";
-const LoadingNewIssuePage = () => {
-  return (
-    <Box className="max-w-xl">
-      <Skeleton height={"2rem"} className="mb-2" />
-      <Skeleton height={"20rem"} />
-    </Box>
-  );
-};
-
-export default LoadingNewIssuePage;
+import IssueFormSkeleton from "../_components/IssueFormSkeleton";
+export default IssueFormSkeleton;
