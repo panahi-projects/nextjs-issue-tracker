549fb857e8bd9a55a82622416d266b9f34048598
SAEED.PANAHI <panahi.projects@gmail.com>
2024-03-30 12:11:28 +0330

    Build an API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -27,3 +27,17 @@ export async function PATCH(
 
   return NextResponse.json(updatedIssue, { status: 200 });
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const issue = await prisma.issue.findUnique({ where: { id: params.id } });
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  await prisma.issue.delete({ where: { id: params.id } });
+
+  return NextResponse.json({}, { status: 200 });
+}
